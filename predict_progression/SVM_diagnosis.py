cd2b6810b9f6281778a5fa48d54385deafd75ed1
roxdumitriu <rox.dumitriu@gmail.com>
2018-10-24 12:21:14 +0100

    Add an extra SVM to predict Parkinson's Diagnosis rather than the UPDRS Score

--- predict_progression/SVM_diagnosis.py
+++ predict_progression/SVM_diagnosis.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from sklearn import preprocessing
 from sklearn import svm
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
 from sklearn.model_selection import StratifiedShuffleSplit
 
-N_SPLITS = 1
-data = pd.read_csv("../data/updrs.csv")
+N_SPLITS = 3
+data = pd.read_csv("../data/diagnosis.csv")
 le = preprocessing.LabelEncoder()
 
-X = data.drop(columns=["patno", "score", "date_scan"])
-y = data["score"].astype(int)
+X = data.drop(columns=["patno", "date_scan", "diagnosis"])
+y = data["diagnosis"]
+le.fit(y)
+# 0 = Healthy, 1 = PD, 2 = PRODROMA
+y = le.transform(y)
 
 skf = StratifiedShuffleSplit(n_splits=N_SPLITS)
 fscores = []
@@ -22,6 +25,8 @@ for train_index, test_index in skf.split(X, y):
     y_pred = svclassifier.predict(X_test)
     fscore = f1_score(y_test, y_pred, average='macro')
     fscores.append(fscore)
+    print(confusion_matrix(y_test, y_pred))
+    print(accuracy_score(y_test, y_pred))
 
 average_accuracy = sum(fscores)/N_SPLITS
 print('Average fscore: {0:0.2f}'.format(average_accuracy))
---
+++ predict_progression/data_diagnosis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+diagnosis_df = pd.read_csv("../data/Patient_Status.csv")
+thick_vol_df = pd.read_csv("../data/thickness_and_volume_data.csv")
+
+diagnosis_df.rename(columns={"RECRUITMENT_CAT": "diagnosis", "PATNO": "patno"},
+                    inplace=True)
+diagnosis_df = diagnosis_df[["patno", "diagnosis"]]
+diagnosis_df['patno'] = diagnosis_df['patno'].apply(str)
+
+thick_vol_df['patno'] = thick_vol_df['patno'].apply(str)
+thick_vol_df = pd.merge(thick_vol_df, diagnosis_df, on=["patno"])
+
+print(thick_vol_df["diagnosis"].value_counts())
+
+thick_vol_df.to_csv("../data/diagnosis.csv")
+
