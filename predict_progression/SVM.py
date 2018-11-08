3490a621f2af11a03076ce9b8f94c498abf6e436
roxdumitriu <rox.dumitriu@gmail.com>
2018-11-08 15:41:39 +0000

    Add a Moca score

--- predict_progression/SVM.py
+++ predict_progression/SVM.py
@@ -4,7 +4,7 @@ from sklearn import svm
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
 
 N_SPLITS = 10
-data = pd.read_csv("../data/updrs.csv")
+data = pd.read_csv("../data/moca.csv")
 X = data.drop(columns=["patno", "score", "date_scan"])
 y = data["score"].astype(int)
 
@@ -12,7 +12,7 @@ scaler = preprocessing.StandardScaler().fit(X)
 X = pd.DataFrame(scaler.transform(X))
 skf = StratifiedShuffleSplit(n_splits=N_SPLITS)
 
-SVClassifier = svm.SVC(C=1.5, kernel='linear')
+SVClassifier = svm.SVC(kernel='linear')
 scores = cross_val_score(SVClassifier, X, y,
                          cv=StratifiedShuffleSplit(n_splits=N_SPLITS),
                          scoring='f1_micro')
